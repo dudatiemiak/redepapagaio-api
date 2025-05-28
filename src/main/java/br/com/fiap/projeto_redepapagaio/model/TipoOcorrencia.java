d9aa252b5436d8d0fd3e475481e640efcce3f935
dudatiemiak <rm554756@fiap.com.br>
2025-05-27 22:28:37 -0300

    Adicionando model TipoOcorrencia, rever tipo do atributo nm_tipo_ocorrencia

---
+++ src/main/java/br/com/fiap/projeto_redepapagaio/model/TipoOcorrencia.java
@@ -0,0 +1,24 @@
+package br.com.fiap.projeto_redepapagaio.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "t_ppg_tipo_ocorrencia")
+public class TipoOcorrencia {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idTipoOcorrencia;
+	@NotEmpty(message = "O tipo da ocorrência deve ser informado!")
+    @Size(max = 50, message = "O tipo da ocorrência deve ter no máximo 50 caracteres")
+	@Column(name = "nm_tipo_ocorrencia")
+	private String nmTipoOcorrencia;
+}
