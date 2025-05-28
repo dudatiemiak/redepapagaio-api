d5960d47a8018b28901f5f74332073904c6e5df6
dudatiemiak <rm554756@fiap.com.br>
2025-05-27 21:13:01 -0300

    Primeiro commit do projeto RedePapagaio

---
+++ src/main/java/br/com/fiap/projeto_redepapagaio/ProjetoRedepapagaioApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.projeto_redepapagaio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoRedepapagaioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoRedepapagaioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/projeto_redepapagaio/ProjetoRedepapagaioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.projeto_redepapagaio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoRedepapagaioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
