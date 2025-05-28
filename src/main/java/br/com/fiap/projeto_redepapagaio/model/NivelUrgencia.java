65396a1342d5a9a75318c30bb8476dd1a49887fd
dudatiemiak <rm554756@fiap.com.br>
2025-05-27 22:27:16 -0300

    Adicionando model NivelUrgencia, rever o tipo do nm_nivel

---
+++ src/main/java/br/com/fiap/projeto_redepapagaio/model/NivelUrgencia.java
@@ -0,0 +1,24 @@
+package br.com.fiap.projeto_redepapagaio.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "t_ppg_nivel_urgencia")
+public class NivelUrgencia {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idNivelUrgencia;
+	@NotEmpty(message = "O nível de urgência da ocorrência deve ser informado!")
+    @Size(max = 20, message = "O nível de urgência da ocorrência deve ter no máximo 20 caracteres")
+	@Column(name = "nm_nivel")
+	private String nmNivel;
+}
