b6bf502220bc7d5e655167d28dea0b7fbfb2ab3b
dudatiemiak <rm554756@fiap.com.br>
2025-05-29 17:22:11 -0300

    Adicionando TipoAjudaDTO

--- src/main/java/br/com/fiap/projeto_redepapagaio/dto/TipoAjudaDTO.java
+++ src/main/java/br/com/fiap/projeto_redepapagaio/dto/TipoAjudaDTO.java
@@ -1,5 +1,37 @@
 package br.com.fiap.projeto_redepapagaio.dto;
 
-public class TipoAjudaDTO {
+import org.springframework.hateoas.RepresentationModel;
 
+import br.com.fiap.projeto_redepapagaio.model.TipoAjudaEnum;
+
+public class TipoAjudaDTO extends RepresentationModel<TipoAjudaDTO>{
+	private Long idTipoAjuda;
+	private TipoAjudaEnum nmTipoAjuda;
+	
+	public TipoAjudaDTO() {
+	}
+
+	public TipoAjudaDTO(Long idTipoAjuda, TipoAjudaEnum nmTipoAjuda) {
+		super();
+		this.idTipoAjuda = idTipoAjuda;
+		this.nmTipoAjuda = nmTipoAjuda;
+	}
+
+	public Long getIdTipoAjuda() {
+		return idTipoAjuda;
+	}
+
+	public void setIdTipoAjuda(Long idTipoAjuda) {
+		this.idTipoAjuda = idTipoAjuda;
+	}
+
+	public TipoAjudaEnum getNmTipoAjuda() {
+		return nmTipoAjuda;
+	}
+
+	public void setNmTipoAjuda(TipoAjudaEnum nmTipoAjuda) {
+		this.nmTipoAjuda = nmTipoAjuda;
+	}
+
+	
 }
