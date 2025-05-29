52ab9afa1398aa7a34a654c9eb47a2fcfca38728
dudatiemiak <rm554756@fiap.com.br>
2025-05-29 17:12:57 -0300

    Adicionando StatusOcorrenciaDTO

--- src/main/java/br/com/fiap/projeto_redepapagaio/dto/StatusOcorrenciaDTO.java
+++ src/main/java/br/com/fiap/projeto_redepapagaio/dto/StatusOcorrenciaDTO.java
@@ -1,5 +1,34 @@
 package br.com.fiap.projeto_redepapagaio.dto;
 
-public class StatusOcorrenciaDTO {
+import org.springframework.hateoas.RepresentationModel;
 
+public class StatusOcorrenciaDTO extends RepresentationModel<StatusOcorrenciaDTO>{
+	private Long idStatusOcorrencia;
+	private String nmStatus;
+	
+	public StatusOcorrenciaDTO() {
+	}
+
+	public StatusOcorrenciaDTO(Long idStatusOcorrencia, String nmStatus) {
+		super();
+		this.idStatusOcorrencia = idStatusOcorrencia;
+		this.nmStatus = nmStatus;
+	}
+
+	public Long getIdStatusOcorrencia() {
+		return idStatusOcorrencia;
+	}
+
+	public void setIdStatusOcorrencia(Long idStatusOcorrencia) {
+		this.idStatusOcorrencia = idStatusOcorrencia;
+	}
+
+	public String getNmStatus() {
+		return nmStatus;
+	}
+
+	public void setNmStatus(String nmStatus) {
+		this.nmStatus = nmStatus;
+	}
+	
 }
