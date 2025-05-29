1513c17e267aa97fc04cd05ac435254c87dc93e9
dudatiemiak <rm554756@fiap.com.br>
2025-05-29 10:53:02 -0300

    Adicionando mapper AjudaRealizadaMapper

---
+++ src/main/java/br/com/fiap/projeto_redepapagaio/mapper/AjudaRealizadaMapper.java
@@ -0,0 +1,22 @@
+package br.com.fiap.projeto_redepapagaio.mapper;
+
+import br.com.fiap.projeto_redepapagaio.dto.AjudaRealizadaDTO;
+import br.com.fiap.projeto_redepapagaio.model.AjudaRealizada;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AjudaRealizadaMapper {
+
+    public AjudaRealizadaDTO toDTO(AjudaRealizada ajudaRealizada) {
+
+        AjudaRealizadaDTO dto = new AjudaRealizadaDTO();
+        dto.setIdAjudaRealizada(ajudaRealizada.getIdAjuda());
+        dto.setOcorrencia(ajudaRealizada.getOcorrencia());
+        dto.setTipoAjuda(ajudaRealizada.getTipoAjuda());
+        dto.setUsuario(ajudaRealizada.getUsuario());
+        dto.setDsAjuda(ajudaRealizada.getDsAjuda());
+        dto.setDtAjuda(ajudaRealizada.getDtAjuda());
+        return dto;
+
+    }
+}
