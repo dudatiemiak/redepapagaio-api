a155d6d05bbdcaab6d4c8c0da1f8ba8051e74f3b
dudatiemiak <rm554756@fiap.com.br>
2025-05-29 17:25:36 -0300

    Adicionando TipoOcorrenciaDTO

--- src/main/java/br/com/fiap/projeto_redepapagaio/dto/TipoOcorrenciaDTO.java
+++ src/main/java/br/com/fiap/projeto_redepapagaio/dto/TipoOcorrenciaDTO.java
@@ -1,5 +1,37 @@
 package br.com.fiap.projeto_redepapagaio.dto;
 
-public class TipoOcorrenciaDTO {
+import org.springframework.hateoas.RepresentationModel;
 
+import br.com.fiap.projeto_redepapagaio.model.TipoOcorrenciaEnum;
+
+public class TipoOcorrenciaDTO extends RepresentationModel<TipoOcorrenciaDTO>{
+	private Long idTipoOcorrencia;
+	private TipoOcorrenciaEnum nmTipoOcorrencia;
+	
+	public TipoOcorrenciaDTO() {
+	}
+
+	public TipoOcorrenciaDTO(Long idTipoOcorrencia, TipoOcorrenciaEnum nmTipoOcorrencia) {
+		super();
+		this.idTipoOcorrencia = idTipoOcorrencia;
+		this.nmTipoOcorrencia = nmTipoOcorrencia;
+	}
+
+	public Long getIdTipoOcorrencia() {
+		return idTipoOcorrencia;
+	}
+
+	public void setIdTipoOcorrencia(Long idTipoOcorrencia) {
+		this.idTipoOcorrencia = idTipoOcorrencia;
+	}
+
+	public TipoOcorrenciaEnum getNmTipoOcorrencia() {
+		return nmTipoOcorrencia;
+	}
+
+	public void setNmTipoOcorrencia(TipoOcorrenciaEnum nmTipoOcorrencia) {
+		this.nmTipoOcorrencia = nmTipoOcorrencia;
+	}
+
+	
 }
