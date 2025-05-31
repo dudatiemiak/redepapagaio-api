6f7e2ae1061a3d2a11170296c38b4af2e8d1bf36
dudatiemiak <rm554756@fiap.com.br>
2025-05-31 17:18:35 -0300

    Adicionando TelefoneService

--- src/main/java/br/com/fiap/projeto_redepapagaio/service/TelefoneService.java
+++ src/main/java/br/com/fiap/projeto_redepapagaio/service/TelefoneService.java
@@ -1,5 +1,28 @@
 package br.com.fiap.projeto_redepapagaio.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.fiap.projeto_redepapagaio.dto.TelefoneDTO;
+import br.com.fiap.projeto_redepapagaio.mapper.TelefoneMapperInterface;
+import br.com.fiap.projeto_redepapagaio.model.Telefone;
+
+@Service
 public class TelefoneService {
 
+	@Autowired
+	private TelefoneCachingService cacheT;
+	
+	@Autowired
+	private TelefoneMapperInterface mapperInterfaceT;
+	
+	@Transactional(readOnly = true)
+	public Page<TelefoneDTO> paginar(PageRequest req) {
+		Page<Telefone> telefones = cacheT.findAll(req);
+		Page<TelefoneDTO> telefonesDTO = telefones.map(telefone -> mapperInterfaceT.toDTO(telefone));
+		return telefonesDTO;
+	} 
 }
