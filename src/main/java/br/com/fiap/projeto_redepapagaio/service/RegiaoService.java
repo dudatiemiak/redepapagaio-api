8cf28a3220425d7f3deadcb4e670a56f8aa9b8c8
dudatiemiak <rm554756@fiap.com.br>
2025-05-31 17:10:33 -0300

    Adicionando RegiaoService

--- src/main/java/br/com/fiap/projeto_redepapagaio/service/RegiaoService.java
+++ src/main/java/br/com/fiap/projeto_redepapagaio/service/RegiaoService.java
@@ -1,5 +1,28 @@
 package br.com.fiap.projeto_redepapagaio.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.fiap.projeto_redepapagaio.dto.RegiaoDTO;
+import br.com.fiap.projeto_redepapagaio.mapper.RegiaoMapperInterface;
+import br.com.fiap.projeto_redepapagaio.model.Regiao;
+
+@Service
 public class RegiaoService {
 
+	@Autowired
+	private RegiaoCachingService cacheR;
+	
+	@Autowired
+	private RegiaoMapperInterface mapperInterfaceR;
+	
+	@Transactional(readOnly = true)
+	public Page<RegiaoDTO> paginar(PageRequest req) {
+		Page<Regiao> regioes = cacheR.findAll(req);
+		Page<RegiaoDTO> regioesDTO = regioes.map(regiao -> mapperInterfaceR.toDTO(regiao));
+		return regioesDTO;
+	} 
 }
