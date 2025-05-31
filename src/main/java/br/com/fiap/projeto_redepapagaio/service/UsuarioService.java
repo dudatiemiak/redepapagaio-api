bd5e35a8c1ade3397ff536338bd721b3eee11d56
dudatiemiak <rm554756@fiap.com.br>
2025-05-31 17:31:25 -0300

    Adicionando UsuarioService

--- src/main/java/br/com/fiap/projeto_redepapagaio/service/UsuarioService.java
+++ src/main/java/br/com/fiap/projeto_redepapagaio/service/UsuarioService.java
@@ -1,5 +1,28 @@
 package br.com.fiap.projeto_redepapagaio.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.fiap.projeto_redepapagaio.dto.UsuarioDTO;
+import br.com.fiap.projeto_redepapagaio.mapper.UsuarioMapperInterface;
+import br.com.fiap.projeto_redepapagaio.model.Usuario;
+
+@Service
 public class UsuarioService {
 
+	@Autowired
+	private UsuarioCachingService cacheU;
+	
+	@Autowired
+	private UsuarioMapperInterface mapperInterfaceU;
+	
+	@Transactional(readOnly = true)
+	public Page<UsuarioDTO> paginar(PageRequest req) {
+		Page<Usuario> usuarios = cacheU.findAll(req);
+		Page<UsuarioDTO> usuariosDTO = usuarios.map(usuario -> mapperInterfaceU.toDTO(usuario));
+		return usuariosDTO;
+	} 
 }
