b3ff40d26ce2a86ba1c1c07558b45f060ce31490
dudatiemiak <rm554756@fiap.com.br>
2025-05-31 17:14:41 -0300

    Adicionando StatusOcorrenciaService

--- src/main/java/br/com/fiap/projeto_redepapagaio/service/StatusOcorrenciaService.java
+++ src/main/java/br/com/fiap/projeto_redepapagaio/service/StatusOcorrenciaService.java
@@ -1,5 +1,28 @@
 package br.com.fiap.projeto_redepapagaio.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.fiap.projeto_redepapagaio.dto.StatusOcorrenciaDTO;
+import br.com.fiap.projeto_redepapagaio.mapper.StatusOcorrenciaMapperInterface;
+import br.com.fiap.projeto_redepapagaio.model.StatusOcorrencia;
+
+@Service
 public class StatusOcorrenciaService {
 
+	@Autowired
+	private StatusOcorrenciaCachingService cacheS;
+	
+	@Autowired
+	private StatusOcorrenciaMapperInterface mapperInterfaceS;
+	
+	@Transactional(readOnly = true)
+	public Page<StatusOcorrenciaDTO> paginar(PageRequest req) {
+		Page<StatusOcorrencia> statusOcorrencias = cacheS.findAll(req);
+		Page<StatusOcorrenciaDTO> statusOcorrenciasDTO = statusOcorrencias.map(statusOcorrencia -> mapperInterfaceS.toDTO(statusOcorrencia));
+		return statusOcorrenciasDTO;
+	} 
 }
