59d0b5d071c0edf00c05281534e783336030282a
dudatiemiak <rm554756@fiap.com.br>
2025-05-31 17:04:09 -0300

    Adicionando OcorrenciaCachingService

--- src/main/java/br/com/fiap/projeto_redepapagaio/service/OcorrenciaCachingService.java
+++ src/main/java/br/com/fiap/projeto_redepapagaio/service/OcorrenciaCachingService.java
@@ -1,5 +1,42 @@
 package br.com.fiap.projeto_redepapagaio.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import br.com.fiap.projeto_redepapagaio.model.Ocorrencia;
+import br.com.fiap.projeto_redepapagaio.repository.OcorrenciaRepository;
+
+@Service
 public class OcorrenciaCachingService {
 
+	@Autowired
+	private OcorrenciaRepository repO;
+	
+	@Cacheable(value = "buscarTodosCache")
+	public List<Ocorrencia> findAll(){
+		return repO.findAll();
+	}
+	
+	@Cacheable(value = "buscarPorID", key = "#idOcorrencia")
+	public Optional<Ocorrencia> findById(Long idOcorrencia){
+		return repO.findById(idOcorrencia);
+	}
+	
+	@Cacheable(value = "buscarPaginasOcorrencias", key = "#req")
+	public Page<Ocorrencia> findAll(PageRequest req){
+		return repO.findAll(req);
+	}
+	
+	@CacheEvict(value = {"buscarTodosCache","buscarPorID", "buscarPaginasOcorrencias"},
+			allEntries = true)
+	public void limparCache() {
+		System.out.println("Limpando cache!");
+	}
 }
