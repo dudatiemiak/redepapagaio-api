fad4549143a9268c5397b0f666830c5f20415d29
dudatiemiak <rm554756@fiap.com.br>
2025-05-31 17:06:27 -0300

    Adicionando OcorrenciaService

--- src/main/java/br/com/fiap/projeto_redepapagaio/service/OcorrenciaService.java
+++ src/main/java/br/com/fiap/projeto_redepapagaio/service/OcorrenciaService.java
@@ -1,5 +1,28 @@
 package br.com.fiap.projeto_redepapagaio.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.fiap.projeto_redepapagaio.dto.OcorrenciaDTO;
+import br.com.fiap.projeto_redepapagaio.mapper.OcorrenciaMapperInterface;
+import br.com.fiap.projeto_redepapagaio.model.Ocorrencia;
+
+@Service
 public class OcorrenciaService {
 
+	@Autowired
+	private OcorrenciaCachingService cacheA;
+	
+	@Autowired
+	private OcorrenciaMapperInterface mapperInterfaceO;
+	
+	@Transactional(readOnly = true)
+	public Page<OcorrenciaDTO> paginar(PageRequest req) {
+		Page<Ocorrencia> ocorrencias = cacheA.findAll(req);
+		Page<OcorrenciaDTO> ocorrenciasDTO = ocorrencias.map(ocorrencia -> mapperInterfaceO.toDTO(ocorrencia));
+		return ocorrenciasDTO;
+	} 
 }
