c54a8e105de0b18ed958db48d5e4ebf543dab708
dudatiemiak <rm554756@fiap.com.br>
2025-05-31 17:29:24 -0300

    Adicionando UsuarioCachingService

--- src/main/java/br/com/fiap/projeto_redepapagaio/service/UsuarioCachingService.java
+++ src/main/java/br/com/fiap/projeto_redepapagaio/service/UsuarioCachingService.java
@@ -1,5 +1,42 @@
 package br.com.fiap.projeto_redepapagaio.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import br.com.fiap.projeto_redepapagaio.model.Usuario;
+import br.com.fiap.projeto_redepapagaio.repository.UsuarioRepository;
+
+@Service
 public class UsuarioCachingService {
 
+	@Autowired
+	private UsuarioRepository repU;
+	
+	@Cacheable(value = "buscarTodosCache")
+	public List<Usuario> findAll(){
+		return repU.findAll();
+	}
+	
+	@Cacheable(value = "buscarPorID", key = "#idUsuario")
+	public Optional<Usuario> findById(Long idUsuario){
+		return repU.findById(idUsuario);
+	}
+	
+	@Cacheable(value = "buscarPaginasUsuarios", key = "#req")
+	public Page<Usuario> findAll(PageRequest req){
+		return repU.findAll(req);
+	}
+	
+	@CacheEvict(value = {"buscarTodosCache","buscarPorID", "buscarPaginasUsuarios"},
+			allEntries = true)
+	public void limparCache() {
+		System.out.println("Limpando cache!");
+	}
 }
