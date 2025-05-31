ad153f0dc8902a64a03808092ece65f8bea3d1c3
dudatiemiak <rm554756@fiap.com.br>
2025-05-31 17:23:16 -0300

    Adicionando TipoAjudaService

--- src/main/java/br/com/fiap/projeto_redepapagaio/service/TipoAjudaService.java
+++ src/main/java/br/com/fiap/projeto_redepapagaio/service/TipoAjudaService.java
@@ -1,5 +1,28 @@
 package br.com.fiap.projeto_redepapagaio.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.fiap.projeto_redepapagaio.dto.TipoAjudaDTO;
+import br.com.fiap.projeto_redepapagaio.mapper.TipoAjudaMapperInterface;
+import br.com.fiap.projeto_redepapagaio.model.TipoAjuda;
+
+@Service
 public class TipoAjudaService {
 
+	@Autowired
+	private TipoAjudaCachingService cacheTA;
+	
+	@Autowired
+	private TipoAjudaMapperInterface mapperInterfaceTA;
+	
+	@Transactional(readOnly = true)
+	public Page<TipoAjudaDTO> paginar(PageRequest req) {
+		Page<TipoAjuda> tiposAjudas = cacheTA.findAll(req);
+		Page<TipoAjudaDTO> tiposAjudasDTO = tiposAjudas.map(tipoAjuda -> mapperInterfaceTA.toDTO(tipoAjuda));
+		return tiposAjudasDTO;
+	} 
 }
