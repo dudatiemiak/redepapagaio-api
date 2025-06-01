ea828cc9fa25e8e68c616f7a2b76fb957464dd4a
dudatiemiak <rm554756@fiap.com.br>
2025-05-31 22:33:44 -0300

    Adicionando método para mostrar todas as ajudas realizadas ordenando por data

--- src/main/java/br/com/fiap/projeto_redepapagaio/repository/AjudaRealizadaRepository.java
+++ src/main/java/br/com/fiap/projeto_redepapagaio/repository/AjudaRealizadaRepository.java
@@ -1,9 +1,18 @@
 package br.com.fiap.projeto_redepapagaio.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.fiap.projeto_redepapagaio.model.AjudaRealizada;
 
 public interface AjudaRealizadaRepository extends JpaRepository<AjudaRealizada, Long>{
 
+	// Busca todas as ajudas realizadas ordenadas pela data (mais recente primeiro)
+    @Query(nativeQuery = true, value = """
+        SELECT * FROM t_ppg_ajuda_realizada
+        ORDER BY dt_ajuda DESC
+    """)
+    List<AjudaRealizada> buscarAjudasOrdenadasPorData();
 }
