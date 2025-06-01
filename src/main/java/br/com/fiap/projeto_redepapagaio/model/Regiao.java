594e0b2bd14b545d61c6581d742a299937155646
dudatiemiak <rm554756@fiap.com.br>
2025-05-31 22:55:34 -0300

    Arrumando nome da tabela do banco de dados de regiao

--- src/main/java/br/com/fiap/projeto_redepapagaio/model/Regiao.java
+++ src/main/java/br/com/fiap/projeto_redepapagaio/model/Regiao.java
@@ -12,7 +12,7 @@ import lombok.Data;
 
 @Data
 @Entity
-@Table(name = "t_ppg_ocorrencia")
+@Table(name = "t_ppg_regiao")
 public class Regiao {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
