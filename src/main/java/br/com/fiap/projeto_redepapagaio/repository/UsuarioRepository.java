d0e136be884ec04dd19b6b9bb83956820afa1248
dudatiemiak <rm554756@fiap.com.br>
2025-05-31 22:27:07 -0300

    Adicionando método para ordenar buscar de usuário

--- src/main/java/br/com/fiap/projeto_redepapagaio/repository/UsuarioRepository.java
+++ src/main/java/br/com/fiap/projeto_redepapagaio/repository/UsuarioRepository.java
@@ -1,9 +1,22 @@
 package br.com.fiap.projeto_redepapagaio.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.fiap.projeto_redepapagaio.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 
+	// Busca usuário por substring no nome, email ou senha, ordenado por nome
+    @Query(nativeQuery = true, value = """
+        SELECT * FROM t_ppg_usuario u
+        WHERE u.nm_usuario LIKE CONCAT('%', :sub, '%')
+           OR u.nm_email LIKE CONCAT('%', :sub, '%')
+           OR u.nm_senha LIKE CONCAT('%', :sub, '%')
+        ORDER BY u.nm_usuario ASC
+    """)
+    List<Usuario> buscarUsuarioPorSubstringOrdenadoPorNome(String sub_usuario);
+	
 }
